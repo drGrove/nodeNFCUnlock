a83dd14b4ce06d1130ef9ef2b2e5e10869a5dc43
Danny Grove <danny@drgrovellc.com>
2013-11-27 09:00:10 -0500

    Adds config file.

---
+++ config.template.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var config = {};
+config.postgres = {};
+config.postgres.user = "username";
+config.postgres.host = "localhost";
+config.postgres.password = "password";
+config.postgres.database = "database";
+
+module.exports = {
+    config: config
+}
